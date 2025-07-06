3f294db653a6eafd582a085860a3e6aa19cfaf0d
CoderXYZ14 <s1404islam@gmail.com>
2025-07-07 03:44:45 +0530

    Website- page not found added

---
+++ src/components/index.ts
@@ -0,0 +1,26 @@
+import FloatingGradient from "./FloatingGradient";
+import Header from "./Header";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import HowItWorks from "./HowItWorks";
+import Features from "./Features";
+import BookDemo from "./BookDemo";
+import Demo from "./Demo";
+import Testimonials from "./Testimonials";
+import FAQ from "./FAQ";
+import CTASection from "./CTASection";
+import Pricing from "./Pricing";
+export {
+  Header,
+  Footer,
+  HeroSection,
+  HowItWorks,
+  Features,
+  BookDemo,
+  Demo,
+  Testimonials,
+  FAQ,
+  CTASection,
+  FloatingGradient,
+  Pricing,
+};
